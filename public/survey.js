57fc0c82e2bbe38c79e13af9c3995c05ca72f400
unknown <you@example.com>
2018-01-01 17:56:49 -0800

    survey.js ajax addition

--- public/survey.js
+++ public/survey.js
@@ -1,16 +1,59 @@
 //click event listener for submit button
 
-//get value of name input 
+    //get value of name input 
     //use id = name
-
-//get value of photo input
+    $("#name").val("");
+    
+    //get value of photo input
     //use id = photo
+    $("#photo").val("");
+    
+$(".submit").on("click", function () {
+    event.preventDefault();
 
-//get value of survey inputs
+    //12 make values into object to post
+    var friend = {
+        name: $("#name").val().trim(),
+        photoURL: $("#photo").val().trim(),
+        surveyResponses: [     //put survey here?
+            $("#q1").val().trim(),
+            $("#q2").val().trim(),
+            $("#q3").val().trim(),
+            $("#q4").val().trim(),
+            $("#q5").val().trim(),
+            $("#q6").val().trim(),
+            $("#q7").val().trim(),
+            $("#q8").val().trim(),
+            $("#q9").val().trim(),
+            $("#q10").val().trim()
+    ]
+};
+
+
+    //get value of survey inputs
     //use i
     //use jquery.each to loop through select inputs
-    //push to array
 
-//12 make values into object to post
+    //push to array
+    
+    function survey () {
+        var survey = [];
+        for (i = 1; i <= 10; i++) {
+            var ans = $("#q" + 1).val().trim();
+
+            survey.push(ans);
+        }
+        return survey;
+    }
+
+});
+
+
+
 
 //use ajax
+
+$.post("/api/friends", friend).done(function(data) {
+    $("#");   //location to post? Modal?
+    $("#");   //attr it for source of photo
+});
