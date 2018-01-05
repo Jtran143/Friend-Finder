84395727d7ac0caae96a7f7e4318841287a664cd
unknown <you@example.com>
2018-01-04 18:14:04 -0800

    heroku push and changes to server.js

--- public/survey.js
+++ public/survey.js
@@ -1,50 +1,53 @@
 //click event listener for submit button
+console.log("script is running");
 
     //get value of name input 
     //use id = name
-    $("#name").val("");
+    // $("#name").val("");
     
-    //get value of photo input
-    //use id = photo
-    $("#photo").val("");
+    // //get value of photo input
+    // //use id = photo
+    // $("#photo").val("");
     
-$(".submit").on("click", function () {
+$("#myBtn").on("click", function () {
     event.preventDefault();
-
+    console.log("button click");
     //12 make values into object to post
     var friend = {
         name: $("#name").val().trim(),
         photoURL: $("#photo").val().trim(),
         surveyResponses: [     //put survey here?
-            $("#q1").val().trim(),
-            $("#q2").val().trim(),
-            $("#q3").val().trim(),
-            $("#q4").val().trim(),
-            $("#q5").val().trim(),
-            $("#q6").val().trim(),
-            $("#q7").val().trim(),
-            $("#q8").val().trim(),
-            $("#q9").val().trim(),
-            $("#q10").val().trim()
+        //     $("#q1").val().trim(),
+        //     $("#q2").val().trim(),
+        //     $("#q3").val().trim(),
+        //     $("#q4").val().trim(),
+        //     $("#q5").val().trim(),
+        //     $("#q6").val().trim(),
+        //     $("#q7").val().trim(),
+        //     $("#q8").val().trim(),
+        //     $("#q9").val().trim(),
+        //     $("#q10").val().trim()
     ]
 };
 
+// $("button").click(function(){
+//     $("li").each(function(){
+//         alert($(this).text())
+//     });
+// });
+    $(".chosen-select").each(function() {
+
+        friend.surveyResponses.push($(this).val().trim());
+        console.log($(this));
+    });
+    console.log(friend.surveyResponses);
 
     //get value of survey inputs
     //use i
     //use jquery.each to loop through select inputs
 
     //push to array
-    
-    function survey () {
-        var survey = [];
-        for (i = 1; i <= 10; i++) {
-            var ans = $("#q").val().trim();  //is this right?
 
-            survey.push(ans);
-        }
-        return survey;
-    }
 
 });
 
@@ -53,7 +56,7 @@ $(".submit").on("click", function () {
 
 //use ajax
 
-$.post("/api/friends", friend).done(function(data) {
-    $("#");   //location to post? Modal?
-    $("#");   //attr it for source of photo
-});
+// $.post("/api/friends", friend).done(function(data) {
+//     $("#");   //location to post? Modal?
+//     $("#");   //attr it for source of photo
+// });
--- routing/htmlRoutes.js
+++ routing/htmlRoutes.js
@@ -3,15 +3,18 @@ var path = require("path");
 module.exports = function(app) {
     //home route
     app.get("/", function(req, res) {
-        res.sendFile(path.join(__dirname, "./public/home.html"));
+        res.sendFile(path.join(__dirname, "../public/home.html"));
     });
     //survey route
     app.get("/survey", function(req, res) {
-        res.sendFile(path.join(__dirname, "./public/survey.html"));
+        res.sendFile(path.join(__dirname, "../public/survey.html"));
+    });
+    app.get("/survey.js", function(req, res) {
+        res.sendFile(path.join(__dirname, "../public/survey.js"));
     });
     //if no matching route thenm default to home
     app.get("*", function(req, res) {
-        res.sendFile(path.join(__dirname, "./public/home.html"));
+        res.sendFile(path.join(__dirname, "../public/home.html"));
     });
 
 };
